b8a2ae54967db02eee9c7762130b01ac759342b0
jacklmfa0 <145318594+jacklmfa0@users.noreply.github.com>
2024-12-14 19:01:23 +0000

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AircraftPerformanceCalculator from './AircraftPerformanceCalculator';
+import './App.css';
+import './index.css'; // Add Tailwind CSS
+
+function App() {
+  return (
+    <div className="App">
+      <AircraftPerformanceCalculator />
+    </div>
+  );
+}
+
+export default App;
